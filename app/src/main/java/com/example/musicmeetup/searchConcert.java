5aaab5d3163e16484dc5accba456e72b08728e52
dariamartin <daria.martinn@gmail.com>
2019-03-13 03:47:45 -0500

    new ui with new user interface

---
+++ app/src/main/java/com/example/musicmeetup/searchConcert.java
@@ -0,0 +1,13 @@
+package com.example.musicmeetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searchConcert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_concert);
+    }
+}
